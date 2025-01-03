48675ab1408937b18a422ff52b66626ae47d8e2d
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-03 12:09:37 +0800

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -23,3 +23,32 @@ function getHumanChoice() {
         return "scissors"
     else return "invalid choice" 
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("You win! Rock beats Scissors.")
+        humanScore++
+    } else if (humanChoice == "paper" && computerChoice == "rock"){
+        console.log ("You win! Paper beats Rock.")
+        humanScore++
+    } else if (humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("You win! Scissors beats Paper.")
+        humanScore++
+    } else if (humanChoice == "scissors" && computerChoice == "rock"){
+        console.log("You lose! Rock beats Scissors.")
+        computerScore++
+    } else if (humanChoice == "rock" && computerChoice == "paper"){
+        console.log ("You lose! Paper beats Rock.")
+        computerScore++
+    } else if (humanChoice == "paper" && computerChoice == "scissors"){
+        console.log("You lose! Scissors beats Paper.")
+        computerScore++
+    } else if (humanChoice == "rock" && computerChoice == "rock")
+        console.log("It's a tie! Both of you have chosen Rock.")
+    else if (humanChoice == "paper" && computerChoice == "paper")
+        console.log("It's a tie! Both of you have chosen Paper.")
+    else if (humanChoice == "scissors" && computerChoice == "scissors.")
+        console.log("It's a tie! Both of you have chosen Scissors")
+    else if (humanChoice == "invalid choice")
+        console.log("BOO")
+}
